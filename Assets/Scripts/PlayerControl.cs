d728cab2bb242c25541d36f12b8fb1f93e99f894
shwoomgloom <svasilides2@gmail.com>
2023-11-06 07:48:20 -0500

    Player can move
    -Player can now move.
    -Began work on picking up packages (level selection).

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	//floats
+	public float _playerSpeed = 6f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+        //Gets player postion on screen
+        Vector3 _playerPos = Camera.main.WorldToViewportPoint(
+            transform.position);
+
+        //4-directional movement (top--down)
+        if (Input.GetAxisRaw("Vertical") > 0 &&
+            _playerPos.y < 0.96f)//up
+        {
+            transform.position += new Vector3(
+                0, _playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0 &&
+            _playerPos.y > 0.03f) //down
+        {
+            transform.position -= new Vector3(
+                0, _playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0 &&
+            _playerPos.x < 0.96f) //right
+        {
+            transform.position += new Vector3(
+                _playerSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 &&
+           _playerPos.x > 0.03f) //left
+        {
+            transform.position -= new Vector3(
+                _playerSpeed * Time.deltaTime, 0);
+        }
+    }
+}
