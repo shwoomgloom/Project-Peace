9fc798cc4a67c11c0ae31b4f4245070cd28eeedf
shwoomgloom <svasilides2@gmail.com>
2023-11-14 21:54:36 -0500

    Basic Timer working
    -Basic timer works. Will refine it tomorrow in class.

---
+++ Assets/Scripts/Core Mechanics/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Slider timerSlider;
+
+	//Txet
+	public Text timerText;
+
+	//Float
+	public float gameTime;
+
+	//Bool
+	private bool stopTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+		stopTimer = false;
+		timerSlider.maxValue = gameTime;
+		timerSlider.value = gameTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float time = gameTime - Time.time;
+
+		//Second to minuet conversion stuff
+		int minutes = Mathf.FloorToInt(time / 60);
+		int seconds = Mathf.FloorToInt(time - minutes * 60f);
+
+		string textTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+		//If time reaches 0, stop the timer
+		if(time <= 0 )
+		{
+			stopTimer = true;
+		}
+
+		//Continue Timer while its not 0
+		if(stopTimer == false )
+		{
+			timerText.text = textTime;
+			timerSlider.value = time;
+		}
+	}
+}
