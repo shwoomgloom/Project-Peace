5c970ab5f014d9bb8da7a42adab64ed8878611e6
shwoomgloom <svasilides2@gmail.com>
2023-11-17 14:20:25 -0500

    Started Better Menu work
    -Setting up a simple menu.

---
+++ Assets/Scripts/Core Mechanics/MenuManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour {
+
+	//Game Objects
+	//Title screen and menus
+	public GameObject _titleScreen;
+
+	//Button Game Objects
+	public GameObject _startB, _controlB, _creditB;
+	
+	//Misc. Game Objects
+	public GameObject _overlay;
+
+	//Buttons
+
+	// Use this for initialization
+	void Start () {
+		
+		_overlay.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Starts the game
+	public void GameStart()
+	{
+		//activate overlay
+		_overlay.SetActive(true);
+
+		StartCoroutine(LoadLevel());
+	}
+
+	//Shows player the control menu
+	public void Controls()
+	{
+
+	}
+
+    IEnumerator LoadLevel()
+	{
+        for (int i = 0; i <= 255; i += 5)
+        {
+            //fade the overlay out
+            _overlay.GetComponent<Image>().color =
+                new Color(0, 0, 0, i / 255.0f);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        SceneManager.LoadScene("SampleScene");
+    }
+}
